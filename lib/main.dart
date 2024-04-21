314c8f949c5f2e4428544ac6fcb18a7ccb91d9ff
JSVazz <144262237+JSVazz@users.noreply.github.com>
2024-04-20 21:30:40 -0800

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:audio_player/Pages/app.dart';
 import 'package:flutter/material.dart';
+import 'package:audio_player/utils/databasehelper.dart';
 
 void main() {
+  DatabaseHelper.insertDataIntoDatabase(); // Call the static method
   runApp(const MaterialApp(
     title: "Audio Player",
     debugShowCheckedModeBanner: false,
